e8ae86399f168a6af26b6a39168b8f12c468e9f6
Nishandd <nishant.dahiya2000@gmail.com>
2024-07-13 16:50:51 +0530

    Add Button and Input components

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appwrite/auth'
+import { logout } from '../../store/authSlice'
 
 function LogoutBtn() {
-  return (
-    <div>LogoutBtn</div>
-  )
+    const dispath = useDispatch()
+    const logoutHandler = () => {
+        authService.logout().then(() => {
+            dispath(logout())
+        })
+    }
+    return (
+        <button
+            className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+            onClick={LogoutBtn}
+        >Logout</button>
+    )
 }
 
 export default LogoutBtn
