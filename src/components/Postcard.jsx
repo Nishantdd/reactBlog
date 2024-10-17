f1ad3cb152c6147d02e50ca78441d3822baa2c9b
Nishantdd <nishant.dahiya2000@gmail.com>
2024-10-17 19:52:00 +0530

    Implement Login component using react hook form

--- src/components/Postcard.jsx
+++ src/components/Postcard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import appwriteService from "../appwrite/config"
 import { Link } from 'react-router-dom'
+import appwriteService from "../appwrite/config.js"
 
 function Postcard({$id, title, featuredImage}) {
   return (
